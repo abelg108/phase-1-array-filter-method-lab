6c5970d8a08fe9a3a54289f96fed356db9c9ed06
Abel Bokure <abel.ngt@gmail.com>
2022-06-25 15:48:22 -0400

    commit tree e2983208471c083568ebcd71161bd1c92ffd177f

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+// // Code your solution here
+
+function findMatching(drivers, str) {
+    return drivers.filter(function (driver){
+        return driver.toLowerCase() === str.toLowerCase()})
+    }
+
+    function fuzzyMatch(drivers, str){
+        return drivers.filter(function (driver) {return driver.toLowerCase().substring(0, str.length) === str.toLowerCase() })
+    }
+    
+    function matchName(drivers, attribute) {
+        return drivers.filter(function(drivers) {return drivers.name === attribute})
+    }
